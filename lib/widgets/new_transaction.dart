e9690585f4c4dc5435d2e09b3c6e813b071539f3
Larbi Azzoug <lazzoug@apple.com>
2023-12-02 17:45:19 -0800

    Added Navigator of context pop to close Modal Bottom Sheet after submitting new Transaction

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -26,6 +26,7 @@ class _NewTransactionState extends State<NewTransaction> {
       titleController.text,
       double.parse(amountController.text),
     );
+    Navigator.of(context).pop();
   }
 
   @override
